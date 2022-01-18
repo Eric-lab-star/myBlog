3d2f545a9592096278f8f370cf7b67ed6c8e06c4
Eric <cyon256@icloud.com>
2022-01-18 23:30:38 +0900

    notion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import express from "express";
-
+import "dotenv/config";
+import { Client } from "@notionhq/client";
 const app = express();
-console.log("heelo");
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+const database_id = process.env.NOTION_DATABASE_ID;
+
+const getPage = async () => {
+  const payload = {
+    path: `databases/${database_id}/query`,
+    method: "POST",
+  };
+  const { results } = await notion.request(payload);
+
+  const pages = results.map((page) => {
+    console.log(page.properties.Description.rich_text);
+  });
+};
+getPage();
+
 app.listen(4000, () => console.log("i am listening"));
